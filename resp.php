ef6cf8d6d9fb880baf09dca92bd64f51cdf8f943
Satyam1203 <nakulbegana191170@gmail.com>
2020-01-18 12:32:33 +0530

    Payment setup working

---
+++ resp.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* import checksum generation utility
+*/
+require_once("PaytmKit/lib/encdec_paytm.php");
+
+$paytmChecksum = "";
+
+/* Create a Dictionary from the parameters received in POST */
+$paytmParams = array();
+foreach($_POST as $key => $value){
+	if($key == "CHECKSUMHASH"){
+		$paytmChecksum = $value;
+	} else {
+		$paytmParams[$key] = $value;
+	}
+}
+
+/**
+* Verify checksum
+* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+*/
+$isValidChecksum = verifychecksum_e($paytmParams, "OiE7EE3r_3NTz34A", $paytmChecksum);
+if($isValidChecksum == "TRUE") {
+	echo "Checksum Matched";
+} else {
+	echo "Checksum Mismatched";
+}
+
+?>
