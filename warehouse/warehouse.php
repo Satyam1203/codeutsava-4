ce58125d713b7c3636d11ba0c6ff988ecdce4553
Chandra Shekhar <csahu3932@gmail.com>
2020-01-19 06:00:37 +0530

    warehouse updated

--- warehouse/warehouse.php
+++ warehouse/warehouse.php
@@ -7,26 +7,14 @@
 <body>
 		
 		<form name="f1" action="./add_warehouse.php">
-			<?php
-			     if(isset($_REQUEST['deleted']) && $_REQUEST['deleted']==0)
-            {
-                  echo '<div class="msgBox">';
-                  echo "<h4>Contact add sucessfully.</h4>";
-                  echo '</div>';
-            }
-
-			?>
-               <h4>Enter detail of the warehouse member</h4>   
-			Name: <input type="text" name="wName"><br>
-
-			Address:<input type="text" name="wAddress"><br>
-
-			Contact Number: <input type="number" name="wContact"><br>
-
-			Email: <input type="text" name="wEmail"><br>
-
-			<input type="submit" value="submit">
-			<a href='./show_contact.php'>show</a>
+			<h4>Enter detail of the warehouse Owner</h4>   
+			<div><label>Name</label><input type="text" name="wName"></div>
+			<div><label>Address</label><input type="text" name="wAddress"></div>
+			<div> <label>Contact Number</label> <input type="number" name="wContact"></div>
+			<div><label> Email <label><input type="text" name="wEmail"></div>
+			<div><label>Password <label><input type="password" name="password"></div>
+			<div> <input type="submit" value="submit"></div>
+		 <a href='./show_contact.php'>show</a>
 		</form>
 </body>
 </html>
